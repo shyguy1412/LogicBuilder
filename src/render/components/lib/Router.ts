a7787624dc80c1d1ea0fa85fdaf7f868130db097
Shy <shyguyx1412@gmail.com>
2025-04-29 23:31:57 +0000

    refactor router

--- src/render/components/lib/Router.ts
+++ src/render/components/lib/Router.ts
@@ -1,24 +1,27 @@
 import { createStore } from "@xstate/store";
 import { useSelector } from "@xstate/store/react";
 import { h } from "preact";
-import { useCallback, useMemo } from "preact/hooks";
+import { useCallback } from "preact/hooks";
 
 
 export type View = () => h.JSX.Element;
+
 export type Router<R extends string> = ReturnType<
   typeof createRouter<RouteTable<R>>
 >;
 export type RouteTable<R extends string = string> = { [route in R]: View };
+
 export function createRouter<R extends RouteTable>(
-  router: R,
+  routeTable: R,
   initial: keyof R,
 ) {
+
   const store = createStore({
-    context: { route: initial, view: router[initial] },
+    context: { route: initial, view: routeTable[initial] },
     on: {
       setRoute: (_, event: { route: keyof R; }) => ({
         route: event.route,
-        view: router[event.route],
+        view: routeTable[event.route],
       }),
     },
   });
