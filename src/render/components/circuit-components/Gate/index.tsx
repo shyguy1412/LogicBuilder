5c4086d3a3842475af7e58bcbee2f72dac6440dc
Shy <shyguyx1412@gmail.com>
2025-05-02 14:40:39 +0000

    refactor gate

--- src/render/components/circuit-components/Gate/index.tsx
+++ src/render/components/circuit-components/Gate/index.tsx
@@ -1,17 +1,36 @@
-import { DragTarget } from "@/lib/components/DragNDrop";
 import { GridDraggable } from "@/lib/components/GridSurface";
+import { useControlledState } from "@/lib/components/hooks";
+import { Lumber } from "@/lib/log/Lumber";
+import { Point } from "@/lib/types/Geometry";
 import { h } from "preact";
+import { memo } from "preact/compat";
 
-type Props = { x: number; y: number };
+type Props = {
+  x?: number;
+  y?: number;
+  onPosUpdate?: (pos: Point) => void;
+};
 
-export function Gate({ x, y }: Props) {
+export const Gate = memo(({ onPosUpdate, ...props }: Props) => {
+  Lumber.log(Lumber.RENDER, "GATE RENDER");
+
+  //this lets a Gate manage it position internally.
+  //Position updates from the parent take precedence but dont need to happen
+  //for a component to move
+  const [{ x, y }, setPos] = useControlledState(
+    (x, y) => ({ x: x ?? 0, y: y ?? 0 }),
+    [props.x, props.y],
+    onPosUpdate,
+  );
+
   return (
     // <DragTarget group="a" data={{}}>
     <GridDraggable
       width={3}
       height={5}
-      x={x}
-      y={y}
+      x={x ?? 0}
+      y={y ?? 0}
+      // onPosUpdate={setPos}
     >
       <div
         style={{
@@ -26,4 +45,4 @@ export function Gate({ x, y }: Props) {
     </GridDraggable>
     // </DragTarget>
   );
-}
+});
