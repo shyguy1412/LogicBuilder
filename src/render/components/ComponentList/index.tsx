3e51592a1eda9c7da4b659ae4b768db931642e5b
Shy <shyguyx1412@gmail.com>
2025-05-03 12:59:45 +0000

    add logging

--- src/render/components/ComponentList/index.tsx
+++ src/render/components/ComponentList/index.tsx
@@ -1,10 +1,12 @@
 import { DROP_GROUPS } from "@/components/App";
 import { Gate } from "@/components/circuit-components/Gate";
 import { DragTarget } from "@/lib/components/DragNDrop";
+import { Lumber } from "@/lib/log/Lumber";
 import { h } from "preact";
 import { memo } from "preact/compat";
 
 export const ComponentList = memo(() => {
+  Lumber.log(Lumber.RENDER, "COMPONENT LIST RENDER");
   return (
     <DragTarget
       group={DROP_GROUPS.CIRCUIT_COMPONENT}
